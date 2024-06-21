4c9eb04c0456fc260a3a814a27a93e3827230f85
ji-somnie <ie8907@ewhain.net>
2024-06-21 10:08:20 +0900

    add dependencies

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ import { response } from './config/response.js';
 import { BaseError } from './config/error.js';
 import { status } from './config/response.status.js';
 
+dotenv.config();    // .env 파일 사용 (환경 변수 관리)
 const app = express();
 const port = 3000;
+
+// server setting - veiw, static, body-parser etc..
+app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
+app.use(cors());                            // cors 방식 허용
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({extended: false})); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 // router setting
 app.use('/temp', tempRouter);
@@ -18,10 +26,11 @@ app.use ((req, res, next) => {
 
 app.use((err, req, res, next) => {
     // 템플릿 엔진 변수 설정
-    res.locals.message = err.message;
+    res.locals.message = err.message;   
     // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
-    res.status(err.data.status).send(response(err.data));
+    console.log("error", err);
+    res.status(err.data.status || status.INTERNAL_SERVER_ERROR).send(response(err.data));
 });
 
 app.listen(port, () => {
